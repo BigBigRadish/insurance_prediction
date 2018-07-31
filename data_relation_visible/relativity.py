0a8da52be1e27b3778a96112abb15cfc71db154c
萝卜 <superradish@outlook.com>
2018-07-31 14:00:39 +0800

    BAOGAO

--- data_relation_visible/relativity.py
+++ data_relation_visible/relativity.py
@@ -17,18 +17,18 @@ from numpy import linalg as la
 def euclidSimilar(inA,inB):  
     return 1.0/(1.0+la.norm(inA-inB))  
 #皮尔逊相关系数  
-def pearsonSimilar(inA,inB):  
+def pearsonSimilar(inA,inB):  #pearson相似系数
     if len(inA)<3:  
         return 1.0  
     return 0.5+0.5*np.corrcoef(inA,inB,rowvar=0)[0][1]  
 #余弦相似度  
-def cosSimilar(inA,inB):  
+def cosSimilar(inA,inB):  #余弦相似性
     inA=np.mat(inA)  
     inB=np.mat(inB)  
     num=float(inA*inB.T)  
     denom=la.norm(inA)*la.norm(inB)  
     return 0.5+0.5*(num/denom)  
-def computecorrelation(x,y):
+def computecorrelation(x,y):#kl散度相似系数
     x_bar=np.mean(x)
     y_bar=np.mean(y)
     SSR=0
